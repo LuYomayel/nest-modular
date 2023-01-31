60cea196ecd4d5dee590ac976af48fdaf706ebe3
Luciano Yomayel <luciano.yomayel@alumnos.frgp.utn.edu.ar>
2023-01-31 20:01:19 -0300

    ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,33 @@
+import * as Joi from 'joi';
 import { HttpService, Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
-import { HttpModule } from '@nestjs/axios';
 import { DatabaseModule } from './database/database.module';
+import { environments } from './environments';
+import config from './config';
 
 @Module({
-  imports: [UsersModule, ProductsModule, HttpModule, DatabaseModule],
+  imports: [
+    UsersModule,
+    ProductsModule,
+    HttpModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
